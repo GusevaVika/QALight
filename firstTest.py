729aa18082c4123c5981684e916546c265c036a6
Vika Guseva <gusevavika.mail@gmail.com>
2020-01-26 19:28:33 +0200

    tests

--- firstTest.py
+++ firstTest.py
@@ -1,2 +1,19 @@
-a = 2
-print(a)
+'''
+ конкатенация строк
+'''
+import random
+cycleStartValue = 0
+list1 = []
+while cycleStartValue <= 10:
+    list1.append(random.randint(1, 100))
+    cycleStartValue = cycleStartValue + 1
+print(list1)
+list1.pop()
+print(list1)
+
+'''
+ конкатенация строк
+'''
+S1 = 'First word '
+S2 = 'second word!'
+print(S1 + S2)
