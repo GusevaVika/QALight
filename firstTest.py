fc0773951d014390577f680c3a5e544ef47ddc53
Vika Guseva <gusevavika.mail@gmail.com>
2020-01-26 20:50:27 +0200

    Home Work

--- firstTest.py
+++ firstTest.py
@@ -1,5 +1,5 @@
 '''
- конкатенация строк
+ task 1
 '''
 import random
 cycleStartValue = 0
@@ -12,8 +12,24 @@ list1.pop()
 print(list1)
 
 '''
- конкатенация строк
+ task 2
 '''
-S1 = 'First word '
-S2 = 'second word!'
+S1 = 'this is '
+S2 = 'how it works'
 print(S1 + S2)
+S3 = 'this is '
+S4 = 'how it works'
+S5 = S3+S4
+print(S5)
+
+'''
+task 3
+'''
+one = 8
+two = 5
+three = 13
+four = 25
+print((one+two)*three)
+print(four*10-two)
+print(three/two)
+print(four-three)
