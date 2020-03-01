2fb904054ca42527931555863d6ccf8ac25d5f21
Vika Guseva <gusevavika.mail@gmail.com>
2020-03-01 16:51:17 +0200

    Module

---
+++ ModuleX.py
@@ -0,0 +1,10 @@
+def prediction():
+    x = input("input your favorite number ")
+    if int(x) <= 9:
+        print("today will be a wonderful day")
+    elif int(x) == 10:
+        print("you will relocate or change your workplace")
+    else:
+        print("you must adapt to the new occurring situations")
+
+prediction()
